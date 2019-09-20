2d88f2f001a05c1e7bedee1b42fc39b4a29b9349
Mikko Ravimo <mikko.ravimo@futurice.com>
2019-09-20 15:47:37 +0300

    Handler for different actions
    And full transcript getter

--- server/index.js
+++ server/index.js
@@ -6,19 +6,51 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+var recording = [];
+
 app.post("/actions", (req, res, next) => {
+	const command = req.body;
 	console.log("Received request: " + JSON.stringify(req.body));
-	res.json(respond('Good day. My name is Lucy and I will be guiding you through your daily standup.'));
+
+	if(command.action === 'start') {
+		res.json(respond('Good day. My name is Lucy and I will be guiding you through your daily standup. Who is present?'));
+	}
+
+	if(command.action === 'dictate') {
+		recording.push(command.payload);
+		res.json(ackResponse());
+	}
+
+	recording.forEach(it =>
+		console.log(it)
+	);
+
+});
+
+app.get("/transcript", (req, res, next) => {
+
+	const sentences = recording;
+	res.json({
+		sentences: sentences
+	});
+
 });
 
 app.listen(port, () => {
  console.log("Server running on port " + port);
 });
 
+function ackResponse() {
+	return {
+		action: "ACK",
+		payload: "received text"
+	}
+}
 
 function respond(message) {
 	return {
-		message: message
+		action: "SAY",
+		payload: message
 	}
 };
 
