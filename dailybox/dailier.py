3ce1ada3b0cfd4019b4404e073fb4367b6c3ace7
JarSeal <kai.forsman@gmail.com>
2019-09-21 14:20:10 +0300

    Testing button press.

--- dailybox/dailier.py
+++ dailybox/dailier.py
@@ -47,8 +47,7 @@ def recordUntilKeyword(board):
             sanitized = text.lower()
             loop = sanitized.find('peacock') == -1
             collected.append(sanitized.replace('peacock',''))
-            board.button.when_pressed()
-            loop = False
+            board.button.when_pressed = print('test')
     board.led.state = Led.OFF
     print("STOP RECORDING")
     return ' '.join(collected)
